70c66b95a20b0c0f41a558430ea69a2c805c7a74
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-18 22:52:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,16 +4,34 @@ var chocolateBars = [
   'kitkat',
   'skittles'
   ]
-  function addElementToBeginningOfArray() {
-    var Golden = ["Issac",
-    "Garret",
-    "Ivan"]
-    Golden = ["Mia",...Golden]
-    return (Golden)
+  function addElementToBeginningOfArray(array, element) {
+    return [element, ...array]
   }
-  function destructivelyAddElementToBeginningOfArray () {
-    var badArray = [1, 2, 3]
-    badArray[5] = 5
-    return (badArray)
+  function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element]
   }
+function destructivelyAddElementToEndOfArray (array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray (array, index){
+ return (array[index]) 
+}
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift()
+  return  array
+}
+function removeElementFromBeginningOfArray (array) {
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray (array) { array.pop()
+return array
+}
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, array.length -1)
   
+}
